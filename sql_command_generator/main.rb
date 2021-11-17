35006463dee665e511128f30fcd4fb197719b8a4
Balazs <balazs.birta@gmail.com>
2021-11-17 13:29:43 +0100

    Smoke test successful

--- sql_command_generator/main.rb
+++ sql_command_generator/main.rb
@@ -1,2 +1,14 @@
-require 'csv'
-require 'erb'
+#create folder
+#create file
+#open file
+#fill file with content
+#close file
+Dir.mkdir('output') unless Dir.exist?('output')
+
+def output_method
+    return 'smoke test'
+end
+
+File.open('output/random_user_entries.txt', 'w') do |file|
+    file.puts output_method
+end
