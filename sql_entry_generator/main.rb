b3ed7a372432135b480d90052c120e510b60ab0f
Balazs <balazs.birta@gmail.com>
2021-11-17 16:55:37 +0100

    Added random name and email generator. Email generator to be updated

--- sql_entry_generator/main.rb
+++ sql_entry_generator/main.rb
@@ -5,10 +5,38 @@
 #close file
 Dir.mkdir('output') unless Dir.exist?('output')
 
-def output_method
-    return 'smoke test'
+def insert_random_detail(filename)
+    if filename == "email"
+        random_text = insert_random_detail("files/first_names.txt") + "." + insert_random_detail("files/last_names.txt") + "@" + "fakemail.com"
+        return random_text
+    end
+    word_array = []
+    #open file, load it's contents into an array
+    File.open(filename).readlines.each do |line|
+        word_array << line.chomp
+    end
+    #filter names that are either too short or too long
+    word_array.select! { |string| string.length > 1 and string.length < 9}
+    #randomly select a name from the array, capitalize if it's a last name
+    filename == "files/last_names.txt" ? random_text = word_array.sample.capitalize : random_text = word_array.sample
+    #implicit return
+    random_text
+end
+
+def generate_entries
+    #starts with 1 because IDs supposed to start with 1
+    i = 1
+    entries = ""
+    #adjust the number of entries by increasing loops
+    while i < 5 do
+        new_entry = "INSERT INTO users (id, first_name, last_name, email, points_overall, validated) VALUES (#{i}, #{insert_random_detail("files/first_names.txt")}, #{insert_random_detail("files/last_names.txt")}, #{insert_random_detail("email")});"
+        entries << new_entry
+        entries << "\r\n"
+    i += 1
+    end
+    entries
 end
 
 File.open('output/random_user_entries.txt', 'w') do |file|
-    file.puts output_method
+    file.puts generate_entries
 end
