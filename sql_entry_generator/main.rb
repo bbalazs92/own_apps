679fac229b916cecdd169284cac336a9e05ee076
Balazs <balazs.birta@gmail.com>
2021-11-30 22:11:24 +0100

    Optimized execution time for larger entries by several magnitudes

--- sql_entry_generator/main.rb
+++ sql_entry_generator/main.rb
@@ -1,19 +1,19 @@
 Dir.mkdir('output') unless Dir.exist?('output')
 
-def insert_random_detail(filename)
-    if filename == "email"
-        random_text = insert_random_detail("files/first_names.txt") + "." + insert_random_detail("files/last_names.txt") + "@" + "fakemail.com"
-        return random_text
-    end
-    word_array = []
-    #open file, load it's contents into an array
-    File.open(filename).readlines.each do |line|
-        word_array << line.chomp
-    end
-    #filter names that are either too short or too long
-    word_array.select! { |string| string.length > 1 and string.length < 9}
+$first_name_array = []
+$last_name_array = []
+
+File.open("files/first_names.txt").readlines.each do |line|
+    $first_name_array << line.chomp
+end
+
+File.open("files/last_names.txt").readlines.each do |line|
+    $last_name_array << line.chomp
+end
+
+def insert_random_detail(names)
     #randomly select a name from the array, capitalize if it's a last name
-    filename == "files/last_names.txt" ? random_text = word_array.sample.capitalize : random_text = word_array.sample
+    names == "files/last_names.txt" ? random_text = $last_name_array.sample.capitalize : random_text = $first_name_array.sample
     #implicit return
     random_text
 end
@@ -24,7 +24,7 @@ def generate_entries
     entries = ""
     #adjust the number of entries by increasing loops
     #note to self: try yield
-    while i < 101 do
+    while i < 1001 do
         new_entry = "INSERT INTO users (id, first_name, last_name, email, points_overall, validated) VALUES (#{i}, \"#{save_first_name = insert_random_detail("files/first_names.txt")}\", \"#{save_last_name = insert_random_detail("files/last_names.txt")}\", \"#{save_first_name + "." + save_last_name + "@fakemail.com"}\", #{rand(300)}, #{true});"
         entries.concat(new_entry).concat("\r\n")
     i += 1
